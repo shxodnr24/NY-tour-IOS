06d83820b626cc048066ff3f137c88499ee5764e
taewook noh <shxodnr24@gmail.com>
2018-04-14 14:53:18 +0900

    Hello
    First Swift

---
+++ UpperEast 복사본/UpperEastFirst.swift
@@ -0,0 +1,67 @@
+//
+//  UpperEastFirst.swift
+//  NewYorkTour
+//
+//  Created by Taewook Noh on 2018. 4. 4..
+//  Copyright © 2018년 taewook. All rights reserved.
+//
+
+import UIKit
+
+class UpperEastFirst: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let mainMenu = ["Tourist Attraction", "Restaurant", "profile", "settings"]
+    let image = ["IMG_6512.JPG", "IMG_7025.JPG", "IMG_7497.JPG", "IMG_5022.JPG"]
+    override func viewDidLoad() {
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+     return mainMenu.count
+        
+    }
+    
+   
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! UpperEastFirstCell
+       
+        cell.imageCell.image = UIImage(named: image[indexPath.row])
+        //cell.imageCell.image = UIImage(named: mainMenu[indexPath.row])
+        cell.labelCell.text = mainMenu[indexPath.row].capitalized
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let name = mainMenu[indexPath.row]
+        let viewController = storyboard?.instantiateViewController(withIdentifier: name)
+         self.navigationController?.pushViewController(viewController!, animated: true)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
